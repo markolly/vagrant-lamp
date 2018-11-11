d9522d12e6159b523c8b3466898afa697a6affdf
markolly <mark@oliver.cx>
2018-11-11 23:39:53 +0000

    Added software-properties-common to provisioning script

--- provision/setup-ubuntu.sh
+++ provision/setup-ubuntu.sh
@@ -5,6 +5,7 @@ echo '[bootstrap] Provisioning virtual machine...'
 echo '[bootstrap] Updating...'
 sudo /usr/bin/apt-get update >> /vagrant/vm_build.log 2>&1
 sudo /usr/bin/apt-get -y upgrade >> /vagrant/vm_build.log 2>&1
+sudo /usr/bin/apt-get -y install software-properties-common 2>&1
 
 # Install puppet
 echo '[bootstrap] Installing Puppet...'
@@ -12,14 +13,6 @@ sudo /usr/bin/wget https://apt.puppetlabs.com/puppet5-release-xenial.deb >> /vag
 sudo /usr/bin/dpkg -i puppet5-release-xenial.deb >> /vagrant/vm_build.log 2>&1
 sudo /usr/bin/apt-get update >> /vagrant/vm_build.log 2>&1
 sudo /usr/bin/apt-get install puppet-agent=5.5.8* >> /vagrant/vm_build.log 2>&1
-
-# Add jenkins-deploy user
-echo '[bootstrap] Adding jenkins-deploy user...'
-sudo /usr/sbin/adduser jenkins-deploy >> /vagrant/vm_build.log 2>&1
-
-# Add deploy group
-echo '[bootstrap] Adding deploy group...'
-sudo /usr/sbin/addgroup deploy >> /vagrant/vm_build.log 2>&1
 
 # Install ip-tables
 echo '[bootstrap] Installing ip-tables...'
